6718898b9f8ce3e7b180a530ce69732b8eb95ef1
HappySonia <soniahuyan@gmail.com>
2022-02-02 09:14:47 +1000

    update

---
+++ index.js
@@ -0,0 +1,35 @@
+// step1: define 9 boxes' name ---add class name
+// step2: define 2 players' inputs
+// step3: define 2 players' scores
+// step4: define the win rules ---> use if condition 
+// step5:
+
+// var arr = ['box1', 2, 3, 4, 5, 6, 7, 8, 9]
+var xScore = 0;
+var OScore = 0;
+var choicePlayer1;
+var choicePlayer2;
+
+
+//play1 first click
+// var randomIndex = Math.floor(Math.random() * 9) + 1;
+// var box = document.querySelectorAll('.box-container div')[randomIndex]
+// console.log(box)
+
+// asign 'X' to the clicked box;
+
+// document.querySelector('.box-container').addEventListener(('click'), function() {
+//     document.querySelectorAll('.box-container div')[randomIndex].textContent = 'X';
+// })
+
+if (choicePlayer1 == 1) {
+    box1.textContent = 'X';
+} else if (choicePlayer1 == 2) {
+    box2.textContent = 'X';
+} else if (choicePlayer3 == 3) {
+    box3.textContent = 'X';
+} else if (choicePlayer4 == 4) {
+    box3.textContent = 'X';
+} else if (choicePlayer3 == 3) {
+    box3.textContent = 'X';
+}
