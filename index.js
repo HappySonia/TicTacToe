d25232b87f47ceccaa1f853dc0239fa7c7879457
HappySonia <soniahuyan@gmail.com>
2022-02-02 16:59:03 +1000

    basic worked

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ var box9 = document.querySelector('.box9')
 var toggle = true;
 document.querySelector('.win').textContent = '';
 
-
 //switch turn works
 function markedNot(box) {
     box.addEventListener('click', function(event) {
@@ -19,12 +18,13 @@ function markedNot(box) {
             if (toggle == true) {
                 event.target.textContent = 'X'
                 toggle = false;
+                win();
             } else {
                 event.target.textContent = 'O'
                 toggle = true;
+                win();
             }
         }
-
     })
 }
 markedNot(box1)
@@ -39,89 +39,109 @@ markedNot(box9)
 
 
 // win condition
-if (box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') {
-    box1.style.backgroundColor = 'green';
-    box2.style.backgroundColor = 'green';
-    box3.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box1.textContent == 'O' && box2.textContent == 'O' && box3.textContent == 'O') {
-    box1.style.backgroundColor = 'green';
-    box2.style.backgroundColor = 'green';
-    box3.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box4.textContent == 'X' && box5.textContent == 'X' && box6.textContent == 'X') {
-    box4.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box6.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box4.textContent == 'O' && box5.textContent == 'O' && box6.textContent == 'O') {
-    box4.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box6.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box7.textContent == 'X' && box8.textContent == 'X' && box9.textContent == 'X') {
-    box7.style.backgroundColor = 'green';
-    box8.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box7.textContent == 'O' && box8.textContent == 'O' && box9.textContent == 'O') {
-    box7.style.backgroundColor = 'green';
-    box8.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box1.textContent == 'X' && box5.textContent == 'X' && box9.textContent == 'X') {
-    box1.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box1.textContent == 'O' && box5.textContent == 'O' && box9.textContent == 'O') {
-    box1.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box3.textContent == 'X' && box5.textContent == 'X' && box7.textContent == 'X') {
-    box3.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box7.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box3.textContent == 'O' && box5.textContent == 'O' && box7.textContent == 'O') {
-    box3.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box7.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box1.textContent == 'X' && box4.textContent == 'X' && box7.textContent == 'X') {
-    box1.style.backgroundColor = 'green';
-    box4.style.backgroundColor = 'green';
-    box7.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box1.textContent == 'O' && box4.textContent == 'O' && box7.textContent == 'O') {
-    box1.style.backgroundColor = 'green';
-    box4.style.backgroundColor = 'green';
-    box7.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box2.textContent == 'X' && box5.textContent == 'X' && box8.textContent == 'X') {
-    box2.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box8.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box2.textContent == 'O' && box5.textContent == 'O' && box8.textContent == 'O') {
-    box2.style.backgroundColor = 'green';
-    box5.style.backgroundColor = 'green';
-    box8.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else if (box3.textContent == 'X' && box6.textContent == 'X' && box9.textContent == 'X') {
-    box3.style.backgroundColor = 'green';
-    box6.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play1 win';
-} else if (box3.textContent == 'O' && box6.textContent == 'O' && box9.textContent == 'O') {
-    box3.style.backgroundColor = 'green';
-    box6.style.backgroundColor = 'green';
-    box9.style.backgroundColor = 'green';
-    document.querySelector('.win').textContent = 'play2 win';
-} else {
-    document.querySelector('.win').textContent = 'you are tie';
+
+function win() {
+    if (box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') {
+        box1.style.backgroundColor = 'green';
+        box2.style.backgroundColor = 'green';
+        box3.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box1.textContent == 'O' && box2.textContent == 'O' && box3.textContent == 'O') {
+        box1.style.backgroundColor = 'green';
+        box2.style.backgroundColor = 'green';
+        box3.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player2 win');
+    } else if (box4.textContent == 'X' && box5.textContent == 'X' && box6.textContent == 'X') {
+        box4.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box6.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box4.textContent == 'O' && box5.textContent == 'O' && box6.textContent == 'O') {
+        box4.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box6.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player1 win');
+    } else if (box7.textContent == 'X' && box8.textContent == 'X' && box9.textContent == 'X') {
+        box7.style.backgroundColor = 'green';
+        box8.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box7.textContent == 'O' && box8.textContent == 'O' && box9.textContent == 'O') {
+        box7.style.backgroundColor = 'green';
+        box8.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player1 win');
+    } else if (box1.textContent == 'X' && box5.textContent == 'X' && box9.textContent == 'X') {
+        box1.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box1.textContent == 'O' && box5.textContent == 'O' && box9.textContent == 'O') {
+        box1.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player2 win');
+    } else if (box3.textContent == 'X' && box5.textContent == 'X' && box7.textContent == 'X') {
+        box3.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box7.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box3.textContent == 'O' && box5.textContent == 'O' && box7.textContent == 'O') {
+        box3.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box7.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player2 win');
+    } else if (box1.textContent == 'X' && box4.textContent == 'X' && box7.textContent == 'X') {
+        box1.style.backgroundColor = 'green';
+        box4.style.backgroundColor = 'green';
+        box7.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');
+    } else if (box1.textContent == 'O' && box4.textContent == 'O' && box7.textContent == 'O') {
+        box1.style.backgroundColor = 'green';
+        box4.style.backgroundColor = 'green';
+        box7.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+    } else if (box2.textContent == 'X' && box5.textContent == 'X' && box8.textContent == 'X') {
+        box2.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box8.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');;
+    } else if (box2.textContent == 'O' && box5.textContent == 'O' && box8.textContent == 'O') {
+        box2.style.backgroundColor = 'green';
+        box5.style.backgroundColor = 'green';
+        box8.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player2 win');;
+    } else if (box3.textContent == 'X' && box6.textContent == 'X' && box9.textContent == 'X') {
+        box3.style.backgroundColor = 'green';
+        box6.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play1 win';
+        alert('player1 win');;
+    } else if (box3.textContent == 'O' && box6.textContent == 'O' && box9.textContent == 'O') {
+        box3.style.backgroundColor = 'green';
+        box6.style.backgroundColor = 'green';
+        box9.style.backgroundColor = 'green';
+        document.querySelector('.win').textContent = 'play2 win';
+        alert('player2 win');;
+    } else {
+        document.querySelector('.win').textContent = 'you are tie';
+        return;
+    }
 }
+
 
 
 // restart button works
