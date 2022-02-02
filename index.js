34b457633dda98dd2314b4aeba85010a62eabe3a
HappySonia <soniahuyan@gmail.com>
2022-02-02 14:26:13 +1000

    some works

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
-// step1: define 9 boxes' name ---add class name
-// step2: define 2 players' inputs
-// step3: define 2 players' scores
-// step4: define the win rules ---> use if condition 
-// step5:
+var box1 = document.querySelector('.box1')
+var box2 = document.querySelector('.box2')
+var box3 = document.querySelector('.box3')
+var box4 = document.querySelector('.box4')
+var box5 = document.querySelector('.box5')
+var box6 = document.querySelector('.box6')
+var box7 = document.querySelector('.box7')
+var box8 = document.querySelector('.box8')
+var box9 = document.querySelector('.box9')
 
-// var arr = ['box1', 2, 3, 4, 5, 6, 7, 8, 9]
-var xScore = 0;
-var OScore = 0;
-var choicePlayer1;
-var choicePlayer2;
+var toggle = true;
+var player1;
+var player2;
 
+//switch turn works
+function markedNot(box) {
+    box.addEventListener('click', function(event) {
+        if (event.target.textContent === '') {
+            if (toggle == true) {
+                event.target.textContent = 'X'
+                toggle = false;
+            } else {
+                event.target.textContent = 'O'
+                toggle = true;
+            }
+        }
 
-//play1 first click
-// var randomIndex = Math.floor(Math.random() * 9) + 1;
-// var box = document.querySelectorAll('.box-container div')[randomIndex]
-// console.log(box)
-
-// asign 'X' to the clicked box;
-
-// document.querySelector('.box-container').addEventListener(('click'), function() {
-//     document.querySelectorAll('.box-container div')[randomIndex].textContent = 'X';
-// })
-
-if (choicePlayer1 == 1) {
-    box1.textContent = 'X';
-} else if (choicePlayer1 == 2) {
-    box2.textContent = 'X';
-} else if (choicePlayer3 == 3) {
-    box3.textContent = 'X';
-} else if (choicePlayer4 == 4) {
-    box3.textContent = 'X';
-} else if (choicePlayer3 == 3) {
-    box3.textContent = 'X';
+    })
 }
+markedNot(box1)
+markedNot(box2)
+markedNot(box3)
+markedNot(box4)
+markedNot(box5)
+markedNot(box6)
+markedNot(box7)
+markedNot(box8)
+markedNot(box9)
+
+
+// win condition
+if (box1.textContent == 'X' && box2.textContent == 'X' && box3.textContent == 'X') {
+    console.log('player1 wins')
+
+}
+
+
+// restart button works
+document.querySelector('.restart').addEventListener('click', function() {
+
+    box1.textContent = '';
+    box2.textContent = '';
+    box3.textContent = '';
+    box4.textContent = '';
+    box5.textContent = '';
+    box6.textContent = '';
+    box7.textContent = '';
+    box8.textContent = '';
+    box9.textContent = '';
+
+})
